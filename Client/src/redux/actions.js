d3680a334b2ac5bfdad6a69ed5d851aed27485e1
MDeFaria <maria.defariasf@gmail.com>
2023-10-05 12:15:35 -0400

    Async-Await

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -14,11 +14,10 @@ export const addFav = (character) => {
 export const removeFav = (id) => {
   const endpoint = "http://localhost:3001/rickandmorty/fav/" + id;
   return async (dispatch) => {
-    axios.delete(endpoint).then(({ data }) => {
-      return dispatch({
-        type: "REMOVE_FAV",
-        payload: data,
-      });
+    const { data } = await axios.delete(endpoint);
+    return dispatch({
+      type: "REMOVE_FAV",
+      payload: data,
     });
   };
 };
